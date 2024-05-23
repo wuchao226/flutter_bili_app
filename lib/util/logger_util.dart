de2230b41116cfc256ee15a99c2dc01991c1e2e9
wuchao <wuchao@apusapps.com>
2024-05-23 12:04:39 +0800

    基于logger封装日志打印工具类

--- lib/util/logger_util.dart
+++ lib/util/logger_util.dart
@@ -6,25 +6,38 @@ class LoggerUtil {
   // 是否输入日志标识
   static bool _isLog = true;
 
-  static final _logger = Logger();
+  static final _logger = Logger(
+    // debug mode 时日志都会被打印
+    filter: DevelopmentFilter(),
+    printer: PrettyPrinter(
+      methodCount: 2,
+      stackTraceBeginIndex: 1,
+    ),
+  );
 
   static void init({bool isLog = false}) {
     _isLog = isLog;
   }
 
-  static void i(String message) {
+  static void i(dynamic message) {
     if (_isLog) {
       _logger.i(message);
     }
   }
 
-  static void d(String message) {
+  static void d(dynamic message) {
     if (_isLog) {
       _logger.d(message);
     }
   }
 
-  static void e(String message) {
+  static void w(dynamic message) {
+    if (_isLog) {
+      _logger.w(message);
+    }
+  }
+
+  static void e(dynamic message) {
     if (_isLog) {
       _logger.e(message);
     }
